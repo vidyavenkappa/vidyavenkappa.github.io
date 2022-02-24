b7a9f18a6879e8e575343de63bf243bbade3e0c9
Vidya Venkappa <vidyavenkappa@gmail.com>
2022-02-24 18:33:03 +0530

    Updates

---
+++ precache-manifest.a9c792489cf6fa212a4384082bbbbb6a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeedfbe235448ad8935564328520c400",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d7f85611fc9d277d477",
+    "url": "/static/css/2.14351861.chunk.css"
+  },
+  {
+    "revision": "b3c05469f2708344ab57",
+    "url": "/static/css/main.d0fe3bb2.chunk.css"
+  },
+  {
+    "revision": "0d7f85611fc9d277d477",
+    "url": "/static/js/2.dc3d6a97.chunk.js"
+  },
+  {
+    "revision": "b5c8281ca89c1ddb89c15ead0e00f32f",
+    "url": "/static/js/2.dc3d6a97.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b3c05469f2708344ab57",
+    "url": "/static/js/main.168423ea.chunk.js"
+  },
+  {
+    "revision": "9d7565089bd47a7e97d4",
+    "url": "/static/js/runtime-main.3bfcc2ab.js"
+  },
+  {
+    "revision": "aa2ddfe2b8a0dfe7415b9d5edbc5697f",
+    "url": "/static/media/hello.aa2ddfe2.png"
+  },
+  {
+    "revision": "8c75c4f4fd1fed478753cc78dfa6cecd",
+    "url": "/static/media/pp.8c75c4f4.jpeg"
+  }
+]);
